62220dead9155e0f89a342aca967a81b0058eab3
ttak0422 <ttak0422@gmail.com>
2024-02-10 23:12:29 +0900

    chore(nvim/statuscol): modify segments

--- neovim/fnl/statuscol.fnl
+++ neovim/fnl/statuscol.fnl
@@ -1,5 +1,6 @@
 (let [M (require :statuscol)
       B (require :statuscol.builtin)
-      segments [{:text [B.lnumfunc] :click "v:lua.ScLa"}
-                {:text [B.foldfunc] :click "v:lua.ScFa"}]]
+      segments [{:text ["%s"] :click "v:lua.ScSa"}
+                {:text [B.lnumfunc] :click "v:lua.ScLa"}
+                {:text [" " B.foldfunc " "] :click "v:lua.ScFa"}]]
   (M.setup {:setopt true :relculright false : segments}))
--- neovim/lua/statuscol.lua
+++ neovim/lua/statuscol.lua
@@ -1,5 +1,5 @@
 -- [nfnl] Compiled from neovim/fnl/statuscol.fnl by https://github.com/Olical/nfnl, do not edit.
 local M = require("statuscol")
 local B = require("statuscol.builtin")
-local segments = {{text = {B.lnumfunc}, click = "v:lua.ScLa"}, {text = {B.foldfunc}, click = "v:lua.ScFa"}}
+local segments = {{text = {"%s"}, click = "v:lua.ScSa"}, {text = {B.lnumfunc}, click = "v:lua.ScLa"}, {text = {" ", B.foldfunc, " "}, click = "v:lua.ScFa"}}
 return M.setup({setopt = true, segments = segments, relculright = false})
