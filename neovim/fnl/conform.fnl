0f10a770de9b2b9f1528bd08ecc518b8d7c834a7
ttak0422 <ttak0422@gmail.com>
2024-03-03 21:42:40 +0900

    chore(nvim/conform): add js/ts formatter

--- neovim/fnl/conform.fnl
+++ neovim/fnl/conform.fnl
@@ -1,4 +1,10 @@
-(let [M (require :conform)
+(let [M (require :conform) ;;
+      ;; WIP
+      is_active_lsp (fn [lsp_name]
+                      (let [bufnr (vim.api.nvim_get_current_buf)
+                            clients (vim.lsp.buf_get_clients bufnr)]
+                        (accumulate [acc false _ client (ipairs clients)]
+                          (or acc (= lsp_name client.name)))))
       formatters_by_ft {:dart [:dart_format]
                         :json [:fixjson]
                         :rust [:rustfmt]
@@ -10,7 +16,10 @@
                         :python [:yapf]
                         :fennel [:fnlfmt]
                         :java [:google-java-format]
-                        :nix [:nixfmt]}]
+                        :nix [:nixfmt]
+                        ;; WIP
+                        :typescript [:prettier]
+                        :javascript [:prettier]}]
   (M.setup {: formatters_by_ft :notify_on_error true :format_on_save false})
   (vim.api.nvim_create_user_command :Format
                                     (fn []
--- neovim/lua/conform.lua
+++ neovim/lua/conform.lua
@@ -1,8 +1,19 @@
 -- [nfnl] Compiled from neovim/fnl/conform.fnl by https://github.com/Olical/nfnl, do not edit.
 local M = require("conform")
-local formatters_by_ft = {dart = {"dart_format"}, json = {"fixjson"}, rust = {"rustfmt"}, shfmt = {"shfmt"}, lua = {"stylua"}, toml = {"taplo"}, html = {"htmlbeautifier"}, yaml = {"yamlfmt"}, python = {"yapf"}, fennel = {"fnlfmt"}, java = {"google-java-format"}, nix = {"nixfmt"}}
+local is_active_lsp
+local function _1_(lsp_name)
+  local bufnr = vim.api.nvim_get_current_buf()
+  local clients = vim.lsp.buf_get_clients(bufnr)
+  local acc = false
+  for _, client in ipairs(clients) do
+    acc = (acc or (lsp_name == client.name))
+  end
+  return acc
+end
+is_active_lsp = _1_
+local formatters_by_ft = {dart = {"dart_format"}, json = {"fixjson"}, rust = {"rustfmt"}, shfmt = {"shfmt"}, lua = {"stylua"}, toml = {"taplo"}, html = {"htmlbeautifier"}, yaml = {"yamlfmt"}, python = {"yapf"}, fennel = {"fnlfmt"}, java = {"google-java-format"}, nix = {"nixfmt"}, typescript = {"prettier"}, javascript = {"prettier"}}
 M.setup({formatters_by_ft = formatters_by_ft, notify_on_error = true, format_on_save = false})
-local function _1_()
+local function _2_()
   return M.format({lsp_fallback = true, async = false})
 end
-return vim.api.nvim_create_user_command("Format", _1_, {})
+return vim.api.nvim_create_user_command("Format", _2_, {})
