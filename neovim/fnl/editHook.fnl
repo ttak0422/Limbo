c318e9dc0797b8090bcf2281d7418190735b1547
ttak0422 <ttak0422@gmail.com>
2024-03-09 00:56:35 +0900

    fixup

--- neovim/fnl/editHook.fnl
+++ neovim/fnl/editHook.fnl
@@ -9,6 +9,15 @@
             ;; 補完オプション
             :completeopt ""
             ;; 短径選択を寛容に
-            :virtualedit :block}]
+            :virtualedit :block}
+      default_ops {:noremap true :silent true}
+      mk_desc (fn [d] {:noremap true :silent true :desc d})
+      cmd (fn [c] (.. :<cmd> c :<cr>))
+      lua_cmd (fn [c] (cmd (.. "lua " c)))
+      ns [[:<leader>U (cmd :UndotreeToggle (mk_desc "toggle undotree"))]]]
+  ;; options ;;
   (each [k v (pairs opts)]
-    (tset vim.o k v)))
+    (tset vim.o k v))
+  ;; keymaps ;;
+  (each [_ k (ipairs ns)]
+    (vim.keymap.set :n (. k 1) (. k 2) (or (. k 3) default_ops))))
--- neovim/lua/editHook.lua
+++ neovim/lua/editHook.lua
@@ -1,6 +1,26 @@
 -- [nfnl] Compiled from neovim/fnl/editHook.fnl by https://github.com/Olical/nfnl, do not edit.
 local opts = {expandtab = true, tabstop = 2, shiftwidth = 2, showmatch = true, completeopt = "", virtualedit = "block"}
+local default_ops = {noremap = true, silent = true}
+local mk_desc
+local function _1_(d)
+  return {noremap = true, silent = true, desc = d}
+end
+mk_desc = _1_
+local cmd
+local function _2_(c)
+  return ("<cmd>" .. c .. "<cr>")
+end
+cmd = _2_
+local lua_cmd
+local function _3_(c)
+  return cmd(("lua " .. c))
+end
+lua_cmd = _3_
+local ns = {{"<leader>U", cmd("UndotreeToggle", mk_desc("toggle undotree"))}}
 for k, v in pairs(opts) do
   vim.o[k] = v
+end
+for _, k in ipairs(ns) do
+  vim.keymap.set("n", k[1], k[2], (k[3] or default_ops))
 end
 return nil
