ce77edc41d7f6098232be1f6551a4c8235937568
ttak0422 <ttak0422@gmail.com>
2024-03-09 21:45:28 +0900

    chore(nvim/oil): modify option

--- neovim/fnl/oil.fnl
+++ neovim/fnl/oil.fnl
@@ -11,13 +11,15 @@
                    :concealcursor :nvic}
       keymaps {:g? :actions.show_help
                :<CR> :actions.select
+               :e :actions.select
                ; :<C-s> :actions.select_vsplit
                ; :<C-h> :actions.select_split
                ; :<C-t> :actions.select_tab
                ; :<C-p> :actions.preview
                :<C-c> :actions.close
-               ; :<C-l> :actions.refresh
-               ; :- :actions.parent
+               :R :actions.refresh
+               :h :actions.parent
+               :l :actions.select
                ; :_ :actions.open_cwd
                ; "`" :actions.cd
                ; "~" :actions.tcd
--- neovim/lua/oil.lua
+++ neovim/lua/oil.lua
@@ -2,8 +2,8 @@
 local M = require("oil")
 local columns = {"icon"}
 local buf_options = {bufhidden = "hide", buflisted = false}
-local win_options = {signcolumn = "number", foldcolumn = "0", conceallevel = 3, concealcursor = "nvic", wrap = false, cursorcolumn = false, list = false, spell = false}
-local keymaps = {["g?"] = "actions.show_help", ["<CR>"] = "actions.select", ["<C-c>"] = "actions.close"}
+local win_options = {signcolumn = "number", foldcolumn = "0", conceallevel = 3, concealcursor = "nvic", wrap = false, list = false, cursorcolumn = false, spell = false}
+local keymaps = {["g?"] = "actions.show_help", ["<CR>"] = "actions.select", e = "actions.select", ["<C-c>"] = "actions.close", R = "actions.refresh", h = "actions.parent", l = "actions.select"}
 local keymaps_help = {border = "rounded"}
 local view_options
 local function _1_(name, bufnr)
@@ -20,4 +20,4 @@ float = {padding = 2, max_width = 0, max_height = 0, border = "single", win_opti
 local preview = {max_width = 0.9, min_width = {40, 0.4}, width = nil, max_height = 0.9, min_height = {5, 0.1}, height = nil, border = "single", win_options = {winblend = 0}, update_on_cursor_moved = true}
 local progress = {max_width = 0.9, min_width = {40, 0.4}, width = nil, max_height = {10, 0.9}, min_height = {5, 0.1}, height = nil, border = "single", minimized_border = "none", win_options = {winblend = 0}}
 local ssh = {border = "single"}
-return M.setup({delete_to_trash = true, prompt_save_on_select_new_entry = true, cleanup_delay_ms = 2000, constrain_cursor = "editable", columns = columns, buf_options = buf_options, win_options = win_options, keymaps = keymaps, keymaps_help = keymaps_help, float = float, preview = preview, progress = progress, ssh = ssh, experimental_watch_for_changes = false, use_default_keymaps = false, lsp_rename_autosave = false, skip_confirm_for_simple_edits = false, default_file_explorer = false})
+return M.setup({delete_to_trash = true, prompt_save_on_select_new_entry = true, cleanup_delay_ms = 2000, constrain_cursor = "editable", columns = columns, buf_options = buf_options, win_options = win_options, keymaps = keymaps, keymaps_help = keymaps_help, float = float, preview = preview, progress = progress, ssh = ssh, default_file_explorer = false, use_default_keymaps = false, lsp_rename_autosave = false, skip_confirm_for_simple_edits = false, experimental_watch_for_changes = false})
