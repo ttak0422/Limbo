98990e5bef966ee4b00f48f60a50d341fdaf6deb
ttak0422 <ttak0422@gmail.com>
2024-03-10 10:36:43 +0900

    chore(nvim/direnv): change silent option

--- neovim/fnl/direnv.fnl
+++ neovim/fnl/direnv.fnl
@@ -1,2 +1,2 @@
 (set vim.g.direnv_auto 1)
-(set vim.g.direnv_silent_load 1)
+(set vim.g.direnv_silent_load 0)
--- neovim/lua/direnv.lua
+++ neovim/lua/direnv.lua
@@ -1,4 +1,4 @@
 -- [nfnl] Compiled from neovim/fnl/direnv.fnl by https://github.com/Olical/nfnl, do not edit.
 vim.g.direnv_auto = 1
-vim.g.direnv_silent_load = 1
+vim.g.direnv_silent_load = 0
 return nil
