1d2c5175f2d1c497b12f275f490658386f9a768f
ttak0422 <ttak0422@gmail.com>
2024-04-07 21:00:06 +0900

    fix(nix): typo

--- modules/hm/darwin/default.nix
+++ modules/hm/darwin/default.nix
@@ -4,7 +4,7 @@ _: {
       # https://texwiki.texjp.org/?Mac#defaultkeybinding-dict
       file."/Library/KeyBindings/DefaultKeyBinding.dict".text =
         builtins.readFile ./DefaultKeyBinding.dict;
-      file."Library/LaunchAgents/Neovide.plist".text =
+      file."/Library/LaunchAgents/Neovide.plist".text =
         builtins.readFile ./Neovide.plist;
     };
   };
