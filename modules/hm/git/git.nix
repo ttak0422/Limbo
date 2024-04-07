d999ca3e88c2f3c6469f2f034ab3eaed59c477aa
ttak0422 <ttak0422@gmail.com>
2024-04-07 14:15:37 +0900

    chore(git): setup commit template

--- modules/hm/git/git.nix
+++ modules/hm/git/git.nix
@@ -39,39 +39,39 @@
       };
       color = { ui = "auto"; };
       init.defaultBranch = "main";
-      # commit.template = pkgs.writeText "committemplate" ''
-      #
-      #
-      #   # = HEADER =================================================
-      #   #
-      #   # build: ---------------- changes that affect artifacts
-      #   # feat: ----------------- add new feature
-      #   # chore: ---------------- maintenance
-      #   # fix: ------------------ bug fix
-      #   # refactor: ------------- refactor
-      #   # test: ----------------- test
-      #   # style: ---------------- formatting
-      #   # docs: ----------------- documents
-      #   # perf: ----------------- improve performance
-      #   # ci: ------------------- ci
-      #   #
-      #   # <type>(<scope>): <short summary>
-      #   #   │       │             │
-      #   #   │       │             └─> Summary in present tense.
-      #   #   │       │                 Not capitalized.
-      #   #   │       │                 No period at the end.
-      #   #   │       │
-      #   #   │       └─> Commit Scope: common|core|logic|...
-      #   #   │
-      #   #   └─> Commit Type: build|feat|chore|...
-      #   #
-      #   # = FOOTER (if exists) =====================================
-      #   #
-      #   # - BREAKING CHANGE: <breaking change summary>
-      #   # - DEPRECATED: <what is deprecated>
-      #   #
-      #   # ==========================================================
-      # '';
+      commit.template = "${pkgs.writeText "committemplate" ''
+
+
+        # = HEADER =================================================
+        #
+        # build: ---------------- changes that affect artifacts
+        # feat: ----------------- add new feature
+        # chore: ---------------- maintenance
+        # fix: ------------------ bug fix
+        # refactor: ------------- refactor
+        # test: ----------------- test
+        # style: ---------------- formatting
+        # docs: ----------------- documents
+        # perf: ----------------- improve performance
+        # ci: ------------------- ci
+        #
+        # <type>(<scope>): <short summary>
+        #   │       │             │
+        #   │       │             └─> Summary in present tense.
+        #   │       │                 Not capitalized.
+        #   │       │                 No period at the end.
+        #   │       │
+        #   │       └─> Commit Scope: common|core|logic|...
+        #   │
+        #   └─> Commit Type: build|feat|chore|...
+        #
+        # = FOOTER (if exists) =====================================
+        #
+        # - BREAKING CHANGE: <breaking change summary>
+        # - DEPRECATED: <what is deprecated>
+        #
+        # ==========================================================
+      ''}";
     };
   };
   home.packages = with pkgs; [ ghq pre-commit gh ];
