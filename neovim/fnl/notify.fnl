ccc206236715da8b0e15cdface15fe3175048caf
ttak0422 <ttak0422@gmail.com>
2024-05-01 07:25:52 +0900

    chore(nvim): change notification position

--- neovim/fnl/notify.fnl
+++ neovim/fnl/notify.fnl
@@ -1,6 +1,6 @@
 (let [M (require :notify)]
   (M.setup {:timeout 2500
-            :top_down true
+            :top_down false
             :stages :static
             :background_colour "#000000"})
   (set vim.notify M))
--- neovim/lua/notify.lua
+++ neovim/lua/notify.lua
@@ -1,5 +1,5 @@
 -- [nfnl] Compiled from neovim/fnl/notify.fnl by https://github.com/Olical/nfnl, do not edit.
 local M = require("notify")
-M.setup({timeout = 2500, top_down = true, stages = "static", background_colour = "#000000"})
+M.setup({timeout = 2500, stages = "static", background_colour = "#000000", top_down = false})
 vim.notify = M
 return nil
