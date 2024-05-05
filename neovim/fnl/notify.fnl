ceb08626d9eea0ca21854b6381f182fb2352fd70
ttak0422 <ttak0422@gmail.com>
2024-05-05 23:20:41 +0900

    chore(nvim/notify): modify render

--- neovim/fnl/notify.fnl
+++ neovim/fnl/notify.fnl
@@ -1,5 +1,6 @@
 (let [M (require :notify)]
   (M.setup {:timeout 2500
+            :render :wrapped-compact
             :top_down false
             :stages :static
             :background_colour "#000000"})
--- neovim/lua/notify.lua
+++ neovim/lua/notify.lua
@@ -1,5 +1,5 @@
 -- [nfnl] Compiled from neovim/fnl/notify.fnl by https://github.com/Olical/nfnl, do not edit.
 local M = require("notify")
-M.setup({timeout = 2500, stages = "static", background_colour = "#000000", top_down = false})
+M.setup({timeout = 2500, render = "wrapped-compact", stages = "static", background_colour = "#000000", top_down = false})
 vim.notify = M
 return nil
