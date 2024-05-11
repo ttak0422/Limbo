e3a9518d70af221496d948d443b0bf9da5255ec8
ttak0422 <ttak0422@gmail.com>
2024-05-11 15:11:15 +0900

    chore(nvim/sonic): update template

--- neovim/snip/sonic/lua/base-nvim-local-config.lua
+++ neovim/snip/sonic/lua/base-nvim-local-config.lua
@@ -7,6 +7,9 @@
 
 -- shared ----------------------------------------------------------------------
 
+-- environment variables
+-- e.g. vim.env.FOO = "foo"
+
 -- `FILE_NAME` replace to filename
 vim.g.sonic_license_header = nil
 
