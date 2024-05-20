af832e8f82916840031d1574e0625b374b828ffb
ttak0422 <ttak0422@gmail.com>
2024-05-21 00:25:08 +0900

    chore(nvim/glance): modify for performance

--- neovim/fnl/glance.fnl
+++ neovim/fnl/glance.fnl
@@ -1,4 +1,4 @@
 (let [M (require :glance)
       border {:enable true :top_char "━" :bottom_char "━"}
-      folds {:fold_closed "▸" :fold_open "▾" :folded false}]
+      folds {:fold_closed "▸" :fold_open "▾" :folded true}]
   (M.setup {: border : folds}))
--- neovim/lua/glance.lua
+++ neovim/lua/glance.lua
@@ -1,5 +1,5 @@
 -- [nfnl] Compiled from neovim/fnl/glance.fnl by https://github.com/Olical/nfnl, do not edit.
 local M = require("glance")
 local border = {enable = true, top_char = "\226\148\129", bottom_char = "\226\148\129"}
-local folds = {fold_closed = "\226\150\184", fold_open = "\226\150\190", folded = false}
+local folds = {fold_closed = "\226\150\184", fold_open = "\226\150\190", folded = true}
 return M.setup({border = border, folds = folds})
