260250613c0f932b9f606d6494ff934f8eab5345
ttak0422 <ttak0422@gmail.com>
2024-06-08 10:51:13 +0900

    chore(nvim/neorg): fat_headlines = false

--- neovim/fnl/headlines.fnl
+++ neovim/fnl/headlines.fnl
@@ -1,2 +1,6 @@
-(let [M (require :headlines)]
-  (M.setup))
+(let [M (require :headlines)
+      markdown {:fat_headlines false}
+      rmd {:fat_headlines false}
+      norg {:fat_headlines false}
+      org {:fat_headlines false}]
+  (M.setup {: markdown : rmd : norg : org}))
--- neovim/lua/headlines.lua
+++ neovim/lua/headlines.lua
@@ -1,3 +1,7 @@
 -- [nfnl] Compiled from neovim/fnl/headlines.fnl by https://github.com/Olical/nfnl, do not edit.
 local M = require("headlines")
-return M.setup()
+local markdown = {fat_headlines = false}
+local rmd = {fat_headlines = false}
+local norg = {fat_headlines = false}
+local org = {fat_headlines = false}
+return M.setup({markdown = markdown, rmd = rmd, norg = norg, org = org})
