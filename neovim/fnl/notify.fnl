a6e0c3852dbcfdfacf418acb3c85f1211a024bac
ttak0422 <ttak0422@gmail.com>
2024-06-12 01:47:14 +0900

    chore(nvim/notify): modify size

--- neovim/fnl/notify.fnl
+++ neovim/fnl/notify.fnl
@@ -1,14 +1,7 @@
-(let [M (require :notify)
-      get_option vim.api.nvim_get_option
-      max_height (fn []
-                   (/ (get_option :lines) 3))
-      max_width (fn []
-                  (* (/ (get_option :columns) 3) 2))]
+(let [M (require :notify)]
   (M.setup {:timeout 2500
             :render :wrapped-compact
             :top_down false
             :stages :static
-            :background_colour "#000000"
-            : max_height
-            : max_width})
+            :background_colour "#000000"})
   (set vim.notify M))
--- neovim/lua/notify.lua
+++ neovim/lua/notify.lua
@@ -1,16 +1,5 @@
 -- [nfnl] Compiled from neovim/fnl/notify.fnl by https://github.com/Olical/nfnl, do not edit.
 local M = require("notify")
-local get_option = vim.api.nvim_get_option
-local max_height
-local function _1_()
-  return (get_option("lines") / 3)
-end
-max_height = _1_
-local max_width
-local function _2_()
-  return ((get_option("columns") / 3) * 2)
-end
-max_width = _2_
-M.setup({timeout = 2500, render = "wrapped-compact", stages = "static", background_colour = "#000000", max_height = max_height, max_width = max_width, top_down = false})
+M.setup({timeout = 2500, render = "wrapped-compact", stages = "static", background_colour = "#000000", top_down = false})
 vim.notify = M
 return nil
